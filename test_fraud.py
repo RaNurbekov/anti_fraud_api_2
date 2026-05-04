918d7c8f7ee87311b73ee773d8f5e79d70fb1014
Ra <nurbekovrashideducation@gmail.com>
2026-05-04 10:54:48 +0500

    Add Redis Feature Store and A/B Testing Router for ML models

--- test_fraud.py
+++ test_fraud.py
@@ -10,8 +10,9 @@ print(json.dumps(resp_1.json(), indent=4, ensure_ascii=False))
 
 
 print("\n--- ТЕСТ 2: Хакерская транзакция (Скомпрометированная карта) ---")
-# Хакер проверяет карту (списывает 0.00)
 payload_2 = {"card_id": "card_hacked_002", "amount": 0.00}
+resp_2 = requests.post(url, json=payload_2)
+print(json.dumps(resp_2.json(), indent=4, ensure_ascii=False))
 
 print("\n--- ТЕСТ 3: Проверка A/B Роутера (10 транзакций) ---")
 # Спамим запросами, чтобы увидеть, как трафик делится между Моделями А и Б
